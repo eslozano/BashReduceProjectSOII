b690bfd080c3231828b1f19bd4f229214a5b8ef0
raul <mirarodriguezraul0812@gmail.com>
2016-01-28 15:34:08 -0500

    modificado script no factoriza, eleva al cuadrado, agregado resultados y screens

--- Factorizacion/script.py
+++ Factorizacion/script.py
@@ -2,15 +2,11 @@
 
 import sys
 
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+def cuadrado(n):
+        return n * n
 
 for line in sys.stdin:
        if line != '\n':
           num = int(line)
-          fact = factorial(num)
-          print fact
+          cuad = cuadrado(num)
 
